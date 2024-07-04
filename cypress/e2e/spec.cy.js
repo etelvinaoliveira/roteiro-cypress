c1423becfec461ca11050cb275f8c3b27332a9de
Etelvina <etelvina.oliveira2003@gmail.com>
2024-07-03 21:26:19 -0300

    Testes de sistema

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -29,6 +29,34 @@ describe('template spec', () => {
       .should('have.length', 0);
   });
 
+  it('Insere e conclui tarefas', () => {
+    cy.visit('http://127.0.0.1:7001');
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.contains('Completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.contains('Clear completed').click();
+    cy.contains('All').click();
+    cy.get('.todo-list li')
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'Prova de ES');
+  });
+
   it('Filtra tarefas completas e ativas', () => {
     cy.visit('http://127.0.0.1:7001'); 
 
@@ -55,5 +83,51 @@ describe('template spec', () => {
     cy.contains('All').click();
     cy.get('.todo-list li')
       .should('have.length', 2);
+  });
+
+  it('Verifica contagem de items restantes', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-count')
+      .should('contain', 2);
+
+    cy.get('.todo-list li .destroy')
+      .first()
+      .invoke('show')
+      .click()
+
+    cy.get('.todo-count')
+      .should('contain', 1);
+
+    cy.get('.todo-list li .toggle')
+      .first()
+      .click();
+
+    cy.get('.todo-count')
+      .should('contain', 0);
+  });
+
+  it('Verifica edicao tarefas', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+
+    cy.get('.todo-list li')
+      .first()
+      .dblclick()
+      .get('.edit')
+      .clear()
+      .type('TP3 de ES{enter}')
+    
+    cy.contains('All').click();
+    cy.get('.todo-list li')
+      .first()
+      .should('have.text', 'TP3 de ES')
   });
 });
